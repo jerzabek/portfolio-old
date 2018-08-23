ab1b4e35f4da5fa0ad57b04509ec8caf92230aa7
Ivan Jerzabek <jarzaimc@gmail.com>
2018-08-23 16:25:44 +0200

    patch
    changed port to fix heroku crashing

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/404', function (req, res) {
 })
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
 })
 
