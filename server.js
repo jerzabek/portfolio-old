ec8e05f335a964f70d3ab437ecd321bd12bd0204
Ivan Jerzabek <jarzaimc@gmail.com>
2018-08-23 16:08:44 +0200

    added content

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.get('/about', function (req, res) {
+  res.render('about/index');
+})
+
+app.get('/404', function (req, res) {
+  res.render('404/index');
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+app.get('*', function (req, res) {
+  res.render('404/index');
+})
